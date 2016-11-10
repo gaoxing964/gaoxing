ec8226cbf1350b4e2251bd74ca2aee9c963a5038
Teng Gao (高腾) <Teng.Gao@spreadtrum.com>
2016-11-10 11:10:36 +0800

    long time no see

--- purePython/test.py
+++ purePython/test.py
@@ -1,7 +1,18 @@
-__author__ = 'rd-gao.teng'
-
-name = raw_input('what is your name ?')
-print 'Hello'+name+'!'
-
-
+import socket
+ 
+HOST, PORT = '', 8888
+ 
+listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+listen_socket.bind((HOST, PORT))
+listen_socket.listen(1)
+print 'Serving HTTP on port %s ...' % PORT
+while True:
+    client_connection, client_address = listen_socket.accept()
+    request = client_connection.recv(1024)
+    print request
+ 
+    http_response = """HTTP/1.1 200 OKHello, World!"""
+    client_connection.sendall(http_response)
+    client_connection.close()
 
